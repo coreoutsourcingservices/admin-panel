ca760c1648aa327e3296381c8d7d9523521b789f
prahlad800 <prahlad80098724678@gmail.com>
2026-05-20 14:57:17 +0530

    fix

--- backend/src/utils/getOTPbyEmail.js
+++ backend/src/utils/getOTPbyEmail.js
@@ -8,13 +8,17 @@ dotenv.config();
 export const sendEmailOTP = async (to, otp) => {
     try {
         const transporter = nodemailer.createTransport({
-             host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL,
                 pass: process.env.APP_PASSWORD
+            },
+            tls: {
+                rejectUnauthorized: false
             }
+
         });
         await transporter.sendMail({
             from: `"HivraSoft" <${process.env.EMAIL}>`,
@@ -99,9 +103,9 @@ export const sendEmailOTP = async (to, otp) => {
         });
         return true;
 
-        
+
     } catch (error) {
 
-      
+
     };
 }
