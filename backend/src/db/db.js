cf9a25d59f83d67725931a3d107b128a0146f5d8
prahlad800 <prahlad80098724678@gmail.com>
2026-05-20 13:03:13 +0530

    fix

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -4,7 +4,7 @@ export const connentDB=()=>{
     const url=process.env.DATA_BASE
      mongoose.connect(url)
     .then(()=>{
-        console.log("mongoose connect...")
+        console.log("mongoose connected")
     })
     .catch((e)=>{
         console.log("mongoose connect  error ",e)
