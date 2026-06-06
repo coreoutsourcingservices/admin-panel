144411480ae9140237df2f6f72b58924d70ffe84
prahlad800 <prahlad8009872468@gmail.com>
2026-06-06 14:54:29 +0530

    backend host hostinger

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,1 +1,2 @@
-export const backendUrl="https://admin-panel-fawn-iota.vercel.app" // backend deploy vercel
+export const backendUrl="https://aliceblue-swan-648054.hostingersite.com"// hostinger deploy
+//"https://admin-panel-fawn-iota.vercel.app" // backend deploy vercel
