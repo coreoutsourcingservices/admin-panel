e4d4bead6362b53f557124e7a80ac2e348cfd201
prahlad800 <prahlad8009872468@gmail.com>
2026-06-12 15:16:28 +0530

    fnkshfd

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,2 +1,1 @@
-export const backendUrl="https://aliceblue-swan-648054.hostingersite.com"// hostinger deploy
-//"https://admin-panel-fawn-iota.vercel.app" // backend deploy vercel
+export const backendUrl= "https://admin-panel-fawn-iota.vercel.app" // backend deploy vercel //"https://aliceblue-swan-648054.hostingersite.com"// hostinger deploy
